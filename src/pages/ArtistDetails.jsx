592a9ebfec7bfe9671f5ea5561718fcd3b11193d
Waruggy <cwang8413@yahoo.com>
2023-02-27 14:06:57 -0800

    My first commit

---
+++ src/pages/ArtistDetails.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
+
+const ArtistDetails = () => {
+  /*Every artist is unique so it is an id for the artists.
+    You can listen to their song if available.
+  */
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
+
+  if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
+
+  if (error) return <Error />;
+
+  //Will return an error if the data from the api fails to get fetched.
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader
+        artistId={artistId}
+        artistData={artistData?.data[0]}
+      />
+
+//Songs that are related to the songs of this artist.
+      <RelatedSongs
+        data={artistData?.data[0].views['top-songs']?.data}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
+};
+
+export default ArtistDetails;
